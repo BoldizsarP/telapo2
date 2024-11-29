1600afe0b4f3d990d10663661747c45183c4ee1b
Boldizsar Pal <boldizsarpal@invenshure.com>
2024-11-29 04:01:33 +0100

    added sanity check

---
+++ telapo/scripts/sanity_check.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function main() {
+  const users = await prisma.user.findMany();
+
+  if (
+    !users.every(
+      (user) =>
+        user.drawsId &&
+        typeof user.drawsId === "string" &&
+        user.drawsId.length !== 0
+    )
+  )
+    throw Error("Some users have no matches");
+  try {
+    Promise.all(
+      users.map(async (user) => {
+        await prisma.user.findFirstOrThrow({ where: { drawsId: user.id } });
+      })
+    );
+  } catch (error) {
+    throw Error("Some users have not been drawn");
+  }
+  console.log("Validated ", users.length);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
